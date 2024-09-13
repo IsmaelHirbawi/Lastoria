f6d3ddef7f8ca0353dc7b7c65a486d01ec245998
IsmaelHirbawi <103568733+IsmaelHirbawi@users.noreply.github.com>
2024-09-13 11:52:21 +0300

    trying to receive notification

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.8.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in your firebaseConfig object
 const firebaseConfig = {
@@ -12,17 +12,20 @@ const firebaseConfig = {
   appId: "1:1048196802674:web:c124bdd30ea3efa4fd3aeb"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Retrieve Firebase Messaging object
+// Retrieve Firebase Messaging
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message ', payload);
+// Handle background notifications
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message: ', payload);
+  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon,
+    icon: payload.notification.icon
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
